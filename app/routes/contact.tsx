1c4557d1705e318a154d0c7aa36a518bc2125fed
JacobBartlett24 <jacobart@ttu.edu>
2023-04-24 12:30:18 -0500

    Add contract information

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,6 +1,7 @@
 import type { LinksFunction } from "@remix-run/node";
 import styles from "~/styles/contact.css"
-
+import {BsMailbox, BsLinkedin} from "react-icons/bs"
+import { Link } from "@remix-run/react";
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }]
 };
@@ -9,20 +10,25 @@ export default function Contact(){
   return(
     <div className="contactPage">
       <h1>Contact Me</h1>
-      <div className="contactCard">
-        <h2>Phone</h2>
-        <p>(806)626-5522</p>
-        <h2>Email</h2>
-        <p>
-          <a href="mailto:">mbart6869@gmail.com</a>
-        </p>
-        <h2>LinkedIn</h2>
-        <p>
-          <a href="https://www.linkedin.com/in/michael-bartlett-1b1b1b1b1/">https://www.linkedin.com/in/michael-bartlett-1b1b1b1b1/</a>
-        </p>
-        <h2>Github</h2>
-        
-
+      <div className="contactWrapper">
+        <div className="contactCard">
+          <div className="cardHeader">
+            Jacob Bartlett
+          </div>
+          <div className="cardBody">
+            <div className="contactMethod">
+              <h2>Phone</h2>
+              <p>(806)626-5522</p>
+            </div>
+            <div className="contactMethod">
+              <h2>Email  <BsMailbox /></h2>
+              <p><a href="mailto:">mbart6869@gmail.com</a></p>
+            </div>
+          </div>
+          <div className="cardFooter">
+            <Link to="https://www.linkedin.com/in/michael-bartlett-1b1b1b1b1/"><BsLinkedin /></Link>
+          </div>
+        </div>
       </div>
     </div>
   )
