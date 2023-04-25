506bc3c4551fc0ce390db7e2197abcdce10a7bf3
JacobBartlett24 <jacobart@ttu.edu>
2023-04-24 21:40:10 -0500

    Add icons to front page

--- app/components/Body.tsx
+++ app/components/Body.tsx
@@ -1,5 +1,9 @@
 import type { LinksFunction } from "@remix-run/node";
 import styles from "../styles/body.css";
+import { FaPython, FaReact } from "react-icons/fa";
+import { SiRemix, SiTypescript } from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io5";
+
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
@@ -9,6 +13,15 @@ export default function Body() {
   return(
     <div className="body">
       <h1 className="portfolioTitle">Jacob Bartlett's <br /> Portfolio</h1>
+      <div className="mainContent">
+        <div className="tools">
+          <FaReact size="300px" color="#61DBFB"/>
+          <SiRemix size="250px" color="red" />
+          <IoLogoJavascript size="300px" color="yellow" />
+          <SiTypescript size="300px" color="#007acc" />
+          <FaPython size="300px" color="red"/>
+        </div>
+      </div>
     </div>
   )
 }
