32a5a2ccc0027a13973ed6f426e16dc016f8cd51
JacobBartlett24 <jacobart@ttu.edu>
2023-04-25 21:39:54 -0500

    Add  route

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -1,7 +1,7 @@
 import type { LinksFunction } from '@remix-run/node';
 import styles from '~/styles/project.css'
 import data from '../data/projects.json'
-import { Link } from '@remix-run/react';
+import { Link, Outlet } from '@remix-run/react';
 
 type Project = {
   name: string,
@@ -21,7 +21,25 @@ export default function Projects(){
     <div className='projectPage'>
       <h1 id="title">Projects</h1>
       <div className='projectContainer'>
-
+        <div className='projectBox'>
+          <div id='projectTitle'>
+            Title
+          </div>
+          <div className='projectsNav'>
+            {projects.map((project, index) => {
+              return(
+                <Link key={index} to={`/projects/${project.name}`}>
+                  <div className='projectNav'>
+                    {project.name}
+                  </div>
+                </Link>
+              )
+            })}
+          </div>
+          <div className='projectBody'>
+            <Outlet />
+          </div>
+        </div>
       </div>
     </div>
   )
---
+++ app/routes/projects/$projectName.tsx
@@ -0,0 +1,11 @@
+export function loader(){
+  return {message: "Loading..."}
+}
+
+export default function Project(){
+  return(
+    <div>
+      project page
+    </div>
+  )
+}
