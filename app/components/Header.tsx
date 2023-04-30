5d2cb583a8b6c6fee215363dac1cb120764c0034
JacobBartlett24 <jacobart@ttu.edu>
2023-04-30 17:39:17 -0500

    Add hamburger menu functionality to front page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,19 +3,30 @@ import styles from "../styles/header.css";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { Link, NavLink } from "@remix-run/react";
 import { FaHamburger } from "react-icons/fa";
+import { useState } from "react";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
 };
 
 export default function Header(){
+  const [isDropdownActive, setIsDropdownActive] = useState("")
+
+  function toggleDropdown(){
+    if(isDropdownActive == ""){
+      setIsDropdownActive("active")
+    }else{
+      setIsDropdownActive("")
+    }
+  }
+
   return(
     <>
       <div className="header">
           <div className="menuWrapper">
-            <button className="hamburgerMenu">
+            <button className="hamburgerMenu" onClick={() => toggleDropdown()}>
               <FaHamburger />
-              <div className="dropDown">
+              <div className={`dropDown ${isDropdownActive}`}>
                 <ul>
                   <li>
                     <Link prefetch="intent" to={"/resume"}>
