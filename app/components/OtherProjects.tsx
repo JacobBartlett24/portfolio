dc5c4f76cd53f3ebdec054e7ef35f68068c86b42
JacobBartlett24 <jacobart@ttu.edu>
2023-05-01 21:07:59 -0500

    Add other project div

---
+++ app/components/OtherProjects.tsx
@@ -0,0 +1,17 @@
+export default function OtherProjects(){
+  return(
+    <>
+      <h2 id="OtherProjects">Other Projects</h2>
+      <div className="otherProjects">
+        <div className="top">
+          <div className="projectTile" id="left1">1</div>
+          <div className="projectTile" id="right1">2</div>
+        </div>
+        <div className="bottom">
+          <div className="projectTile" id="left2">3</div>
+          <div className="projectTile" id="right2">4</div>
+        </div>
+      </div>
+    </>
+  )
+}
