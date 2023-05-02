f07260bc616b2b13738414f6c30f45e8c6afe395
JacobBartlett24 <jacobart@ttu.edu>
2023-05-01 21:12:22 -0500

    Add project images

--- app/components/OtherProjects.tsx
+++ app/components/OtherProjects.tsx
@@ -1,15 +1,26 @@
+import data from '../data/projects.json'
+
 export default function OtherProjects(){
+
   return(
     <>
       <h2 id="OtherProjects">Other Projects</h2>
       <div className="otherProjects">
         <div className="top">
-          <div className="projectTile" id="left1">1</div>
-          <div className="projectTile" id="right1">2</div>
+          <div className="projectTile" id="left1">
+            <img src={data.projects[1].image} alt="" />
+          </div>
+          <div className="projectTile" id="right1">
+          <img src={data.projects[2].image} alt="" />
+          </div>
         </div>
         <div className="bottom">
-          <div className="projectTile" id="left2">3</div>
-          <div className="projectTile" id="right2">4</div>
+          <div className="projectTile" id="left2">
+            <img src={data.projects[0].image} alt="" />
+          </div>
+          <div className="projectTile" id="right2">
+            <img src={data.projects[3].image} alt="" />
+          </div>
         </div>
       </div>
     </>
