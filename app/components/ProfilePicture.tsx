ed8d0ac0579dae77f2e9f0d851d7b4dc40b60e79
Jacob Bartlett <96956475+JacobBartlett24@users.noreply.github.com>
2023-05-10 18:32:54 +0000

    Add some overflow fixes

--- app/components/ProfilePicture.tsx
+++ app/components/ProfilePicture.tsx
@@ -6,8 +6,8 @@ export default function ProfilePicture({animateClass} : Props) {
   return(
     <div className="imgHolder">
       <img className={animateClass} id={animateClass ? "" : "profilePicture"}alt="me" src="https://cdn.midjourney.com/fe42195e-b152-49c1-b0ef-b9276f82874a/0_3.png" />
-      <span className={animateClass == "" ? "" : "placeholder"} hidden={animateClass == "" ? true : false}>asd</span>
-      <p id="blurb" hidden={animateClass ? true : false}>
+      <span className={animateClass ? "placeholder" : ""} hidden={animateClass == "" ? true : false}>asd</span>
+      <p id="blurb" className={animateClass ? "changeColor" : ""}>
         Web Development, Software Engineering, Design,<br/> and Data Science
       </p>
     </div>
