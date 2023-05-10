ac8506010dbf03afed4752dd02421df48ac645f5
Jacob Bartlett <96956475+JacobBartlett24@users.noreply.github.com>
2023-05-10 18:18:01 +0000

    Change text color on animation

--- app/components/Body.tsx
+++ app/components/Body.tsx
@@ -46,7 +46,7 @@ useEffect(() => {
         <div className="imgHolder">
             <img className={animateClass} id={animateClass ? "" : "profilePicture"}alt="me" src="https://cdn.midjourney.com/fe42195e-b152-49c1-b0ef-b9276f82874a/0_3.png" />
             <span className={animateClass == "" ? "" : "placeholder"} hidden={animateClass == "" ? true : false}>asd</span>
-            <p id="blurb" hidden={animateClass ? true : false}>
+            <p id="blurb" className={animateClass ? "changeColor" : ""}>
               Web Development, Software Engineering, Design,<br/> and Data Science
             </p>
         </div>
