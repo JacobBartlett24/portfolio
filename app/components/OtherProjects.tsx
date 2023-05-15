ffa710e8af0d5d515da0f7d4d1a85ddb9fd81ed7
JacobBartlett24 <jacobart@ttu.edu>
2023-05-14 19:28:07 -0500

    Add project overlay

--- app/components/OtherProjects.tsx
+++ app/components/OtherProjects.tsx
@@ -1,19 +1,32 @@
 import data from '../data/projects.json'
 import {Link} from '@remix-run/react'
+
 export function Slides(){
   return(
     <div className="firstSlide">
       <Link to={data.projects[1].url}>
         <img src={data.projects[1].image} alt="" />
+        <div className='projectOverlay'>
+          <p>Description: {data.projects[1].description}<br/><br/> Tools: {data.projects[1].tools}</p>
+        </div>
       </Link>
       <Link to={data.projects[2].url}>
         <img src={data.projects[2].image} alt="" />
+        <div className='projectOverlay'>
+          <p>Description: {data.projects[2].description}<br/><br/> Tools: {data.projects[2].tools}</p>
+        </div>
       </Link>
       <Link to={data.projects[0].url}>
         <img src={data.projects[0].image} alt="" />
+        <div className='projectOverlay'>
+          <p>Description: {data.projects[0].description}<br/><br/> Tools: {data.projects[0].tools}</p>
+        </div>
       </Link>
       <Link to={data.projects[3].url}>
         <img src={data.projects[3].image} alt="" />
+        <div className='projectOverlay'>
+          <p>Description: {data.projects[3].description}<br/><br/> Tools: {data.projects[3].tools}</p>
+        </div>
       </Link>
     </div>
   )
